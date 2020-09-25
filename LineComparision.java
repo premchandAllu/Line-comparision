5566612a39ed77bd5694110952367a6af3e207be
premchandAllu <premchandallu1999@gmail.com>
2020-09-25 18:18:02 +0530

    Added: Welcome message is added

---
+++ LineComparision.java
@@ -0,0 +1,10 @@
+package com.Bridgelabz.LineComparision;
+
+public class LineComparision 
+  {
+	public static void main(String [] args)
+	  { 
+		System.out.println("Welocme to Line Comparision Computation Program");
+	  }
+
+}
